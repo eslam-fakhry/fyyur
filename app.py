85281a9fbad2a12526c84f32edb8757b9e9e5941
eslam-fakhry <eslamphakhry@gmail.com>
2020-11-05 03:55:06 +0200

    Add missing attributes to models

--- app.py
+++ app.py
@@ -42,6 +42,9 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean())
+    seeking_description = db.Column(db.Text())
 
     # Add many-to-many relationship with Artist through Show model
     artists = db.relationship(
@@ -61,8 +64,9 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean())
+    seeking_description = db.Column(db.Text())
 
 
 class Show(db.Model):
